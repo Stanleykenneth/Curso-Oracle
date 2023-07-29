372c7eb9df01059f9d9cbcd25a789f7c61140ab7
Kenneth Stanley <89946281+Stanleykenneth@users.noreply.github.com>
2023-07-29 10:27:58 -0300

    [ShopApp] - Criação de Array
    Criação de um array para armazenar as propriedades dos produtos (Descrição, Tamanho e Preço)

--- Atividade/src/ShopApp/ShopApp.java
+++ Atividade/src/ShopApp/ShopApp.java
@@ -9,7 +9,7 @@ public class ShopApp {
     public static void main(String[] args) {
 
         double tax = 0.2;
-        double total;
+        double total = 0.0;
 
         System.out.println("Bem-vindo ao Duke Choise Shop!");
 
@@ -21,6 +21,8 @@ public class ShopApp {
         Clothing item1 = new Clothing();
         Clothing item2 = new Clothing();
 
+        Clothing[] items = {item1, item2, new Clothing(), new Clothing()};
+
         item1.description = "Blue Jacket";
         item1.price = 20.9;
         item1.size = "M";
@@ -28,26 +30,48 @@ public class ShopApp {
         item2.description = "Orange T-Shirt";
         item2.price = 10.5;
         item2.size = "S";
+        
+        items[2].description = "Green Scarf";
+        items[2].price = 5.0;
+        items[2].size = "S";
+        
+        items[3].description = "Blue T-Shirt";
+        items[3].price = 10.5;
+        items[3].size = "S";
+        
 
-        System.out.println("Item 1" + ", " + item1.description + ", " + item1.price + "," + item1.size + ".");
-        System.out.println("Item 2" + ", " + item2.description + ", " + item2.price + ", " + item2.size + ".");
-
-        total = (item1.price + item2.price * 2) * (1 + tax);
-
-        System.out.println("Total = " + Math.round(total));
-
+        // System.out.println("Item 1" + ", " + item1.description + ", " + item1.price + "," + item1.size + ".");
+        // System.out.println("Item 2" + ", " + item2.description + ", " + item2.price + ", " + item2.size + ".");
+        //  total = (item1.price + item2.price * 2) * (1 + tax);
         int measurement = 3;
 
         switch (measurement) {
-            case 1: case 2: case 3:
+            case 1:
+            case 2:
+            case 3:
                 c1.size = "S";
                 break;
-            case 4: case 5: case 6:
+            case 4:
+            case 5:
+            case 6:
                 c1.size = "M";
-            case 7: case 8: case 9:
+            case 7:
+            case 8:
+            case 9:
                 c1.size = "L";
             default:
                 c1.size = "X";
         }
+
+        for (Clothing item : items) {
+            if (c1.size.equals(item.size)) {
+                total = total + item.price;
+                System.out.println("Item" + ", " + item.description + ", " + item.price + ", " + item.size + ".");
+                if(total > 15){break;}
+            }
+
+        }
+        System.out.println("Total = " + Math.round(total));
+
     }
 }
